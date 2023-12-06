d26b4d79e7e912d4725568403622698728ac5391
fa1ez <faiezmalik3252@gmail.com>
2023-12-06 19:02:20 +0500

    first commit

---
+++ src/components/Avatar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Spline from "@splinetool/react-spline";
+
+export default function Avatar() {
+  return (
+    <>
+      <div style={{position:"absolute",top:0 ,right:0,backgroundColor:"silver",overflowX:"hidden"}}>
+        <Spline scene="https://prod.spline.design/IP1WmIrxRZVw6yaV/scene.splinecode" />
+      </div>
+    </>
+  );
+}
