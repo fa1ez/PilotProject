7006da8f8aac6e2478986499cacf88b38f2c97db
fa1ez <faiezmalik3252@gmail.com>
2023-12-17 23:58:42 +0500

    gsap text effects

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 // import './App.css';
 import Avatar from './components/Avatar';
+import gsap from "gsap";
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    let tl = gsap.timeline();
+    tl.from('.welcome',{opacity: 0,})
+    tl.to('.welcome', {
+      y: 50,
+      opacity: 0,
+      stagger :{
+        each : 0.2
+      } 
+    })
+    
+    
+  
+  },[]);
+
   return (
-    <div style={{backgroundColor:"grey",height:"2000px"}}>
-      <Avatar />
+    <div style={{display:'flex',justifyContent:"center",flexDirection:"column"}}>
+      
+      <div style={{backgroundColor:"grey",height:"100vh"}}>
+        {/* <Avatar /> */}
+        <div style={{position:"absolute",top:50,zIndex:7,display:"flex"}}  >
+          {
+            "Welcome to the Home page".split(' ').map((word,index) => {
+              return <h1  className='welcome' key={index}>{word}</h1>
+            })
+          }
+        </div>
+      </div>
+
     </div>
   );
 }
--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -4,7 +4,7 @@ import Spline from "@splinetool/react-spline";
 export default function Avatar() {
   return (
     <>
-      <div style={{position:"absolute",top:0 ,right:0,backgroundColor:"silver",overflowX:"hidden"}}>
+      <div style={{position:"absolute",top:0 ,right:0,backgroundColor:"silver",overflowX:"hidden",zIndex:5}}>
         <Spline scene="https://prod.spline.design/IP1WmIrxRZVw6yaV/scene.splinecode" />
       </div>
     </>
