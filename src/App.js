e1b27edfe747f565fbabc30577a1788a4572ceeb
fa1ez <faiezmalik3252@gmail.com>
2023-12-18 03:05:18 +0500

    text animation updates

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import './App.css';
+import './App.css';
 import Avatar from './components/Avatar';
 import gsap from "gsap";
 import { useEffect } from 'react';
@@ -7,28 +7,31 @@ function App() {
 
   useEffect(() => {
     let tl = gsap.timeline();
-    tl.from('.welcome',{opacity: 0,})
+    tl.from('.welcome',{opacity: 0})
     tl.to('.welcome', {
-      y: 50,
-      opacity: 0,
+      y: 20,
+      opacity: 1,
       stagger :{
-        each : 0.2
+        each : 0.1
       } 
     })
     
-    
+    // Cleanup function
+    return () => {
+      tl.kill(); // Kill the timeline to clear animations
+    };
   
   },[]);
 
   return (
     <div style={{display:'flex',justifyContent:"center",flexDirection:"column"}}>
       
-      <div style={{backgroundColor:"grey",height:"100vh"}}>
-        {/* <Avatar /> */}
-        <div style={{position:"absolute",top:50,zIndex:7,display:"flex"}}  >
+      <div style={{backgroundColor:"#637E76",height:"100vh"}}>
+        <Avatar />
+        <div style={{position:"absolute",top:150,left:30,zIndex:7,display:"flex",maxWidth:"60vw",flexWrap:"wrap"}}  >
           {
             "Welcome to the Home page".split(' ').map((word,index) => {
-              return <h1  className='welcome' key={index}>{word}</h1>
+              return <h1 className='welcome' key={index} style={{color: word === 'Home' ? "white" : "black"}}>{word}&nbsp;</h1>
             })
           }
         </div>
